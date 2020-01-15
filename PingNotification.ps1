c5a7d5da686302cb681c77335611a8ad06a2214c
PSGuy <45128030+ITTech37@users.noreply.github.com>
2020-01-15 11:06:34 +0100

    Add files via upload

---
+++ PingNotification.ps1
@@ -0,0 +1,32 @@
+﻿# This script pings a target of your choice to ensure the good communication between your host and the remote host and displays the result in a native notification. #
+
+# VARIABLES #
+
+$Target = "www.google.com"
+$Ping = Test-Connection -ComputerName $Target -Quiet
+
+# MAIN #
+
+If ($Ping -eq "True") {
+    Add-Type -AssemblyName System.Windows.Forms
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $global:notification = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $notification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+    $notification.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+    $notification.BalloonTipText = "$Target is UP"
+    $notification.BalloonTipTitle = $Test
+    $notification.Visible = $true
+    $notification.ShowBalloonTip(20000)
+} Else {
+    Add-Type -AssemblyName System.Windows.Forms
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $global:notification = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $notification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+    $notification.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Error
+    $notification.BalloonTipText = "$Target is DOWN"
+    $notification.BalloonTipTitle = $Test
+    $notification.Visible = $true
+    $notification.ShowBalloonTip(20000)
+}
